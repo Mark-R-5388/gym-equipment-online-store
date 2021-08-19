449e65a39bbb9309ae13ad88aa8aa78cd6f14b82
Mark-R-5388 <marose5388@gmail.com>
2021-08-19 18:20:42 -0500

    cart cost functionality finished

--- cart.js
+++ cart.js
@@ -4,7 +4,7 @@ document.querySelector('#homepage').addEventListener('click', () => {
 let cartContainer = document.querySelector('#cart-main-container')
 let parsedCart = JSON.parse(localStorage.getItem('cart'))
 let cart = parsedCart
-
+let costContainer = document.querySelector('#cost-container')
 if (cart.length === 0) {
   cartContainer.innerHTML = '<h1>Cart is Empty</h1>'
 }
@@ -12,7 +12,7 @@ if (cart.length === 0) {
 function render(list) {
   list.forEach((item) => {
     // Create Equipment Container
-    let equipmentContainer = createElement('div', 'store-equipment-container')
+    let equipmentContainer = createElement('div', 'cart-equipment-container')
 
     //Create Image Container
     let equipmentImageContainer = createElement(
@@ -64,14 +64,75 @@ function render(list) {
       cartContainer.innerHTML = ''
 
       render(cart)
+      costContainer.innerHTML = ''
+      renderCost(cart)
     })
   })
   if (cart.length === 0) {
     cartContainer.innerHTML = '<h1>Cart is Empty</h1>'
   }
 }
-render(cart)
 
+function renderCost(list) {
+  // Create Text Container
+  let textContainer = createElement('div', 'text-container')
+  let cartTotalText = document.createElement('p')
+  cartTotalText.textContent = 'Cart Total'
+  let taxText = document.createElement('p')
+  taxText.textContent = '10% Tax'
+  let shippingText = document.createElement('p')
+  shippingText.textContent = 'Shipping'
+  let totalText = document.createElement('p')
+  totalText.textContent = 'Total'
+
+  textContainer.appendChild(cartTotalText)
+  textContainer.appendChild(taxText)
+  textContainer.appendChild(shippingText)
+  textContainer.appendChild(totalText)
+
+  // Create Math Container
+  let mathContainer = createElement('div', 'math-container')
+  let cartTotalMath = document.createElement('p')
+
+  cartTotalMath.textContent = '$' + findCartTotal(list)
+  let taxMath = document.createElement('p')
+  taxMath.textContent = `$` + findTaxTotal(findCartTotal(list))
+  let shippingMath = document.createElement('p')
+  shippingMath.textContent = `$15.00`
+  let totalMath = document.createElement('p')
+  totalMath.textContent =
+    `$` + findTotal(findCartTotal(list), findTaxTotal(findCartTotal(list)))
+
+  mathContainer.appendChild(cartTotalMath)
+  mathContainer.appendChild(taxMath)
+  mathContainer.appendChild(shippingMath)
+  mathContainer.appendChild(totalMath)
+
+  costContainer.appendChild(textContainer)
+  costContainer.appendChild(mathContainer)
+  findCartTotal(list)
+}
+
+render(cart)
+renderCost(cart)
+
+// Find Costs
+function findCartTotal(list) {
+  let cartCostArray = list.map((item) => item.price)
+  let cartCost = cartCostArray.reduce((item1, item2) => item1 + item2)
+
+  return parseInt(cartCost).toFixed(2)
+}
+
+function findTaxTotal(amount) {
+  let taxAmount = (amount * 0.1).toFixed(2)
+  return parseInt(taxAmount).toFixed(2)
+}
+function findTotal(total, tax) {
+  let totalAmount = parseInt(total) + parseInt(tax) + 15
+
+  return parseInt(totalAmount).toFixed(2)
+}
 //Create Element
 function createElement(element, className) {
   let newElement = document.createElement(element)
